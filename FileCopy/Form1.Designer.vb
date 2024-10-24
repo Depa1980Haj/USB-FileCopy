a30f6f037ede941199870f379d44855b203b830f
dennis engellage <dennis@engellage.de>
2024-10-24 09:10:27 +0200

    Projektdateien hinzufügen.

---
+++ FileCopy/Form1.Designer.vb
@@ -0,0 +1,166 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Main
+    Inherits System.Windows.Forms.Form
+
+    'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wird vom Windows Form-Designer benötigt.
+    Private components As System.ComponentModel.IContainer
+
+    'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
+    'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
+    'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Main))
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.txtPfad = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btnReset = New System.Windows.Forms.Button()
+        Me.txtUnterverzeichnis = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.btnClose = New System.Windows.Forms.Button()
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.LabelBol = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.txtUSB = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.StatusStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(12, 167)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(457, 48)
+        Me.ProgressBar1.TabIndex = 0
+        Me.ProgressBar1.UseWaitCursor = True
+        Me.ProgressBar1.Visible = False
+        '
+        'txtPfad
+        '
+        Me.txtPfad.Location = New System.Drawing.Point(12, 65)
+        Me.txtPfad.Name = "txtPfad"
+        Me.txtPfad.Size = New System.Drawing.Size(457, 20)
+        Me.txtPfad.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 88)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(109, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Ziel Hauptverzeichnis"
+        '
+        'btnReset
+        '
+        Me.btnReset.Location = New System.Drawing.Point(386, 221)
+        Me.btnReset.Name = "btnReset"
+        Me.btnReset.Size = New System.Drawing.Size(83, 23)
+        Me.btnReset.TabIndex = 3
+        Me.btnReset.Text = "Zurücksetzen"
+        Me.btnReset.UseVisualStyleBackColor = True
+        '
+        'txtUnterverzeichnis
+        '
+        Me.txtUnterverzeichnis.Location = New System.Drawing.Point(12, 12)
+        Me.txtUnterverzeichnis.Name = "txtUnterverzeichnis"
+        Me.txtUnterverzeichnis.Size = New System.Drawing.Size(457, 20)
+        Me.txtUnterverzeichnis.TabIndex = 4
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(9, 35)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(86, 13)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Unterverzeichnis"
+        '
+        'btnClose
+        '
+        Me.btnClose.Location = New System.Drawing.Point(12, 221)
+        Me.btnClose.Name = "btnClose"
+        Me.btnClose.Size = New System.Drawing.Size(75, 23)
+        Me.btnClose.TabIndex = 6
+        Me.btnClose.Text = "Schließen"
+        Me.btnClose.UseVisualStyleBackColor = True
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.LabelBol})
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 259)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(481, 22)
+        Me.StatusStrip1.TabIndex = 7
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'LabelBol
+        '
+        Me.LabelBol.Name = "LabelBol"
+        Me.LabelBol.Size = New System.Drawing.Size(119, 17)
+        Me.LabelBol.Text = "ToolStripStatusLabel1"
+        '
+        'txtUSB
+        '
+        Me.txtUSB.Location = New System.Drawing.Point(12, 118)
+        Me.txtUSB.Name = "txtUSB"
+        Me.txtUSB.Size = New System.Drawing.Size(265, 20)
+        Me.txtUSB.TabIndex = 8
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 141)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(60, 13)
+        Me.Label3.TabIndex = 9
+        Me.Label3.Text = "USB Name"
+        '
+        'Main
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(481, 281)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtUSB)
+        Me.Controls.Add(Me.StatusStrip1)
+        Me.Controls.Add(Me.btnClose)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtUnterverzeichnis)
+        Me.Controls.Add(Me.btnReset)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtPfad)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Main"
+        Me.Text = "USB File Copy"
+        Me.StatusStrip1.ResumeLayout(False)
+        Me.StatusStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents txtPfad As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents btnReset As Button
+    Friend WithEvents txtUnterverzeichnis As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents btnClose As Button
+    Friend WithEvents StatusStrip1 As StatusStrip
+    Friend WithEvents LabelBol As ToolStripStatusLabel
+    Friend WithEvents txtUSB As TextBox
+    Friend WithEvents Label3 As Label
+End Class
---
+++ FileCopy/Form1.vb
@@ -0,0 +1,244 @@
+﻿Imports System.IO
+Imports System.Management
+Imports System.Configuration
+Imports System.Threading.Tasks
+Imports System.Windows.Forms
+Public Class Main
+    Private WithEvents watcher As ManagementEventWatcher
+    Private targetDirectory As String
+    Dim bolCheck As Boolean = False
+    Private trayIcon As NotifyIcon
+    Private trayMenu As ContextMenu
+
+
+
+
+    ' Füge den Fortschrittsbalken als Mitglied der Form-Klasse hinzu
+    Public Sub New()
+        ' Initialisiere die Windows Form-Komponenten (Fortschrittsbalken etc.)
+        InitializeComponent()
+        LabelBol.Text = "Aktiv"
+        Me.txtPfad.Text = My.Settings.Pfad
+        Me.txtUSB.Text = My.Settings.USBName
+        ' Lese den Zielpfad aus den App-Einstellungen
+        targetDirectory = ConfigurationManager.AppSettings("TargetDirectory")
+        If String.IsNullOrEmpty(targetDirectory) Then
+            targetDirectory = "C:\tmp"
+        End If
+
+        ' Überwache USB-Geräte
+        Dim query As New WqlEventQuery("SELECT * FROM Win32_DeviceChangeEvent WHERE EventType = 2")
+        watcher = New ManagementEventWatcher(query)
+        watcher.Start()
+
+        ' Zeige den aktuellen Zielpfad an
+        Console.WriteLine("Aktueller Zielpfad: " & targetDirectory)
+    End Sub
+
+    ' Wenn ein USB-Gerät erkannt wird
+    Private Sub watcher_EventArrived(sender As Object, e As EventArrivedEventArgs) Handles watcher.EventArrived
+        For Each drive As DriveInfo In DriveInfo.GetDrives()
+            If drive.DriveType = DriveType.Removable AndAlso drive.IsReady Then
+                ' Überprüfe den Namen des USB-Sticks
+                If drive.VolumeLabel = "Test" And bolCheck = False Then
+                    bolCheck = True
+                    Console.WriteLine("USB-Stick erkannt: " & drive.Name)
+
+                    AskForSubdirectoryAndCopyFiles(drive.Name)
+                    LabelBol.Text = "Inaktiv"
+                End If
+            End If
+        Next
+    End Sub
+
+    ' Fordere den Benutzer auf, ein Unterverzeichnis einzugeben, und kopiere dann die Dateien
+    Private Sub AskForSubdirectoryAndCopyFiles(usbPath As String)
+        Try
+            Dim subDir As String
+            ' Frage den Benutzer nach einem Unterverzeichnis
+            If Len(Me.txtUnterverzeichnis.Text) > 0 Then
+
+                subDir = Me.txtUnterverzeichnis.Text
+            Else
+                subDir = InputBox("Bitte geben Sie den Namen des Unterverzeichnisses ein:", "Unterverzeichnis wählen", "NeuesUnterverzeichnis")
+                'Me.txtUnterverzeichnis.Text = subDir
+            End If
+            ' Erstelle den vollständigen Zielpfad mit Unterverzeichnis
+            Dim destDir As String = Path.Combine(targetDirectory, subDir)
+
+            ' Stelle sicher, dass das Zielverzeichnis existiert
+            If Not Directory.Exists(destDir) Then
+                Directory.CreateDirectory(destDir)
+            End If
+
+            ' Überprüfe freien Speicherplatz vor dem Kopieren
+            If Not HasEnoughFreeSpace(destDir, usbPath) Then
+                MessageBox.Show("Nicht genügend freier Speicherplatz auf dem Ziellaufwerk!", "Fehler", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                Return
+            End If
+
+            ' Kopiere die Dateien und zeige den Fortschrittsbalken an
+            CopyFilesFromUSBWithProgress(usbPath, destDir)
+
+        Catch ex As Exception
+            Console.WriteLine("Fehler beim Kopieren: " & ex.Message)
+        End Try
+    End Sub
+
+    ' Prüft, ob genügend Speicherplatz vorhanden ist
+    Private Function HasEnoughFreeSpace(destDir As String, sourceDir As String) As Boolean
+        Try
+            Dim destDrive As DriveInfo = New DriveInfo(Path.GetPathRoot(destDir))
+            Dim totalSize As Long = Directory.GetFiles(sourceDir).Sum(Function(f) New FileInfo(f).Length)
+
+            ' Prüfe, ob genügend freier Speicherplatz vorhanden ist
+            If destDrive.AvailableFreeSpace >= totalSize Then
+                Return True
+            Else
+                Return False
+            End If
+        Catch ex As Exception
+            Console.WriteLine("Fehler bei der Speicherplatzprüfung: " & ex.Message)
+            Return False
+        End Try
+    End Function
+
+    ' Kopiert Dateien vom USB-Stick in das Zielverzeichnis und zeigt den Fortschrittsbalken
+
+
+    Private Sub CopyFilesFromUSBWithProgress(usbPath As String, destDir As String)
+        Try
+            Dim fileCount As Integer = 0
+            Dim totalSize As Long = 0
+
+            ' Lade alle Dateien im Verzeichnis des USB-Sticks
+            Dim files As String() = Directory.GetFiles(usbPath)
+            Dim totalFiles As Integer = files.Length
+
+            ' Setze den Fortschrittsbalken auf die Anzahl der Dateien
+            If ProgressBar1.InvokeRequired Then
+                ProgressBar1.Invoke(Sub()
+                                        ProgressBar1.Visible = True
+                                        ProgressBar1.Minimum = 0
+                                        ProgressBar1.Maximum = totalFiles
+                                        ProgressBar1.Value = 0
+                                    End Sub)
+            Else
+                ProgressBar1.Minimum = 0
+                ProgressBar1.Maximum = totalFiles
+                ProgressBar1.Value = 0
+            End If
+
+            ' Kopiere jede Datei und aktualisiere den Fortschrittsbalken
+            For Each file As String In files
+                Dim fileName As String = Path.GetFileName(file)
+                Dim destFile As String = Path.Combine(destDir, fileName)
+
+                ' Kopiere die Datei und überschreibe, wenn sie bereits existiert
+                System.IO.File.Copy(file, destFile, True)
+
+                ' Statistik sammeln
+                fileCount += 1
+                totalSize += New FileInfo(file).Length
+
+                ' Fortschrittsbalken im UI-Thread aktualisieren
+                If ProgressBar1.InvokeRequired Then
+                    ProgressBar1.Invoke(Sub()
+                                            ProgressBar1.Value = fileCount
+                                        End Sub)
+                Else
+                    ProgressBar1.Value = fileCount
+                End If
+            Next
+
+            ' Ausgabe der Anzahl kopierter Dateien und der Gesamtgröße in GB
+            MessageBox.Show("Anzahl kopierter Dateien: " & fileCount & vbCrLf &
+                        "Gesamtgröße: " & (totalSize / (1024 * 1024 * 1024)).ToString("F2") & " GB",
+                        "Kopieren abgeschlossen")
+
+
+            If ProgressBar1.InvokeRequired Then
+                ProgressBar1.Invoke(Sub()
+                                        ProgressBar1.Visible = False
+                                    End Sub)
+            Else
+                ProgressBar1.Visible = False
+            End If
+
+        Catch ex As Exception
+            MessageBox.Show("Fehler beim Kopieren: " & ex.Message, "Fehler", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+
+    Private Sub btnReset_Click(sender As Object, e As EventArgs) Handles btnReset.Click
+        bolCheck = False
+        LabelBol.Text = "Aktiv"
+    End Sub
+
+    Private Sub btnClose_Click(sender As Object, e As EventArgs) Handles btnClose.Click
+        Me.Close()
+    End Sub
+
+
+    Private Sub txtPfad_DoubleClick(sender As Object, e As EventArgs) Handles txtPfad.DoubleClick
+        Dim folderDialog As New FolderBrowserDialog()
+
+        ' Optional: Setze den aktuellen Ordner als Startordner
+        folderDialog.SelectedPath = "C:\"
+
+        ' Zeige den Dialog an und prüfe, ob der Benutzer auf OK geklickt hat
+        If folderDialog.ShowDialog() = DialogResult.OK Then
+            ' Zeige den ausgewählten Ordner in der TextBox an
+            txtPfad.Text = folderDialog.SelectedPath
+            My.Settings.Pfad = Me.txtPfad.Text
+        End If
+    End Sub
+
+
+
+    Private Sub txtUSB_LostFocus(sender As Object, e As EventArgs) Handles txtUSB.LostFocus
+        If Len(Me.txtUSB.Text) > 0 Then
+            My.Settings.USBName = Me.txtUSB.Text
+        End If
+    End Sub
+
+    Private Sub Main_Load(sender As Object, e As EventArgs) Handles Me.Load
+        trayMenu = New ContextMenu()
+        trayMenu.MenuItems.Add("Öffnen", AddressOf Me.ShowForm)
+        trayMenu.MenuItems.Add("Beenden", AddressOf Me.ExitApp)
+
+        ' Erstelle das Tray Icon
+        trayIcon = New NotifyIcon()
+        trayIcon.Text = "USB File Copy"
+        trayIcon.Icon = My.Resources.USB_Icon ' Hier kannst du ein Icon (.ico) hinzufügen
+
+        ' Verknüpfe das Menü und zeige es im System Tray an
+        trayIcon.ContextMenu = trayMenu
+        trayIcon.Visible = True
+
+        ' Hinzufügen des Klick-Ereignisses für das Tray-Icon
+        AddHandler trayIcon.DoubleClick, AddressOf Me.ShowForm
+    End Sub
+
+    Protected Overrides Sub OnResize(e As EventArgs)
+        MyBase.OnResize(e)
+        If Me.WindowState = FormWindowState.Minimized Then
+            Me.Hide()
+            trayIcon.Visible = True
+        End If
+    End Sub
+
+    ' Zeige das Formular wieder und verstecke das Tray Icon
+    Private Sub ShowForm(sender As Object, e As EventArgs)
+        Me.Show()
+        Me.WindowState = FormWindowState.Normal
+        trayIcon.Visible = False
+    End Sub
+
+    ' Beende die Anwendung über das Tray-Menü
+    Private Sub ExitApp(sender As Object, e As EventArgs)
+        trayIcon.Visible = False
+        Application.Exit()
+    End Sub
+End Class
